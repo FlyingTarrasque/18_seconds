fe071422c31f7a1bff04ea17eb5c9ceb3634edb2
pedroHenrique <pedrohenriquerls@gmail.com>
2014-04-13 13:00:37 -0300

    keystore beta para teste do leaderboard

--- config.lua
+++ config.lua
@@ -12,7 +12,8 @@ application = {
     },
     license ={
         google ={
-            key = "TODO: Create the keystore",
+            --Beta key
+            key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAluGTApSLd2UB+ToLAqarTeJSL9IkrQsexFqNk1NEASXztUAthvL4RqS1ZkUpq7NntST4Iq2rEdG27BLf0OmRgEWSeLtSRpswfRwe+X3nDujgHpCbM/80RCdqL3GDKpP4BepwM1AK++yu86CR+P/lUEC44GPfqmG3Gt4Jc7PITefwFdvOeUiaxoZC4pxqA+dzX0Jp+O0YM8XQtcGzDmEHEICrsTfOUlVlI0K5Jc5NhS6FfJksltws/iGrixHNqAWQ3x5CVwgaM0xtwuuK84b18J+2GJyH+8RBCWhPHnDcKCxigAMXnLvtQ2CMJ2iLwhpDmzETxCVGk8/APjhd6AjdKwIDAQAB",
             policy = "serverManaged",
         },
     },   
@@ -21,6 +22,17 @@ application = {
 settings = {
     orientation = {
         default = 'portrait'
+    },
+    android ={
+        googlePlayGamesAppId = "57559416678" -- Your Google Play Games App Id
+    },
+    plugins ={
+        -- key is the name passed to Lua's 'require()'
+        ["CoronaProvider.gameNetwork.google"] = {
+            -- required
+            publisherId = "com.coronalabs",
+        },
     }
+
 }
 
