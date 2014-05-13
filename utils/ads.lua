5bb7ae50abc2274bd7dcd92220436e5302f2bedb
Henrique Hirako <henriquehirako@gmail.com>
2014-05-13 00:25:42 -0300

    adicionando RevMob para teste, configurado pra poder desativar do pelo site

--- utils/ads.lua
+++ utils/ads.lua
@@ -1,11 +1,17 @@
 local ads = require "ads"
+local RevMob = require("utils.revmob")
+
+require("utils.ice")
 require("constants")
 
 local adType = "banner"
 local messages = display.newText('', halfW, 100, "Arial", 12)
+local settings = ice:loadBox("settings")
+local PLACEMENT_IDS = { [REVMOB_ID_IOS] = '5371649e5b9a4c0016c77a4d', [REVMOB_ID_ANDROID] = '537164a8142dfb2626900d26' }
+local REVMOB_IDS = { [REVMOB_ID_IOS] = '5371649e5b9a4c0016c77a49', [REVMOB_ID_ANDROID] = '537164a8142dfb2626900d22' }
 
 local showAds = function()
-	ads.hide()
+	--ads.hide()
 	local adX, adY = 0, display.actualContentHeight-10
 	ads.show(adType, {x=adX, y=adY, interval=30})
 end
@@ -24,6 +30,13 @@ local function adListener(event)
 	end
 end
 
+local revmobListener = function(event)
+	if(event.type == "adClosed") then
+		showAds()
+	end
+  	print("Event: " .. event.type)
+end
+
 Ads = {}
 Ads_mt = { __index = Ads }
 
@@ -38,6 +51,10 @@ function Ads:new()
 	else
 		ads.init(self.provider, self.appIOSID, adListener)
 	end
+	
+	--INIT REVBMOB
+	RevMob.startSession(REVMOB_IDS)
+	
 	setmetatable(self, Ads_mt)
 
 	return self
@@ -53,7 +70,7 @@ function Ads:show()
 	showAds()
 end
 
-function Ads:showFullscreen()
+function Ads:showFullscreenAdmob()
 	print("mostrando interstitial")
 	if ( system.getInfo("platformName") == "Android" ) then
 		local fullscreenIOSID = "ca-app-pub-4502761084086592/3468850462"
@@ -61,6 +78,19 @@ function Ads:showFullscreen()
 	else
 		local fullscreenIOSID = "ca-app-pub-4502761084086592/3468850462"
 		ads.show("interstitial", {interval=3, appId=fullscreenIOSID})
+	end
+end
+
+function Ads:preLoadFullscreenRevMob()
+	self.fullscreen = RevMob.createFullscreen(revmobListener, PLACEMENT_IDS)
+end
+
+function Ads:showFullscreenRevMob()
+	ads.hide()
+	if(self.fullscreen ~= nil) then
+		self.fullscreen:show()
+	else
+		RevMob.showFullscreen(revmobListener, PLACEMENT_IDS)
 	end
 end
 
